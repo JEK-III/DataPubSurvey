89e0c72c040dcdea0196dbb3a9f3363b6fe4b717
John Kratz <John.Kratz@ucop.edu>
2014-05-14 16:26:32 -0700

    Build output .csv with correct columns
    However, only values are ‘NaN’ and ‘False’

--- Anonymize.py
+++ Anonymize.py
@@ -26,7 +26,7 @@ responses = pd.read_csv("DataPubSurveyResponses - Form Responses.csv")
 
 
 
-responses['discipline'] = responses['discipline'].map(DISCIPLINE_AGGREGATION)
+#responses['discipline'] = responses['discipline'].map(DISCIPLINE_AGGREGATION)
 
 
 
@@ -45,8 +45,9 @@ def strip_other(cell, column):
 
 responses_ft = responses.drop(COLUMNS_TO_DROP, axis=1)
 
-for column in CHECKBOX_COLUMNS:
-        
+
+for column in CHECKBOX_COLUMNS:      
+
     # split into individual answers on ','
     filtered_column = responses_ft[column].str.split(", ").dropna()
     
@@ -54,13 +55,23 @@ for column in CHECKBOX_COLUMNS:
     filtered_column = filtered_column.map(lambda x : strip_other(x, column))
  
 #    filtered_column = pd.DataFrame(responses_ft[column])
-    col_to_ans = COLUMN_TO_ANSWERS[column] + ['Other']    
-    print col_to_ans
+    standard_answers = COLUMN_TO_ANSWERS[column] + ['Other']    
+    """
+    check_resp_dict = {}    
+    
+    for answer in standard_answers:
+        mask = filtered_column.isin(standard_answers)
+        check_resp_dict = {':'.join([column, answer]) : filtered_column[mask]}
+ 
+    checkbox_responses = pd.DataFrame(check_resp_dict)
+    """
     checkbox_responses = pd.DataFrame({answer : 
-        filtered_column.apply(lambda x: answer in x) for answer in col_to_ans})
-    
-  
+        filtered_column.apply(lambda x: answer in x) for answer in standard_answers})
+    # rename with question
+    checkbox_responses = checkbox_responses.rename(columns=lambda x: ':'.join([column, x]))
+      
     responses_ft = pd.merge(responses_ft, checkbox_responses, how='left', left_index=True, right_index=True)
-
     
+    
+responses_ft.to_csv('test_output.csv')
 # consolidate sparsely populated disciplines
