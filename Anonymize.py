46604d07fccefb63d1e243949395f31304b3a9aa
John Kratz <John.Kratz@ucop.edu>
2014-05-15 11:09:39 -0700

    Add discipline aggregation
    Add function to aggregate sparsely (>3) disciplines into a broader
    category

--- Anonymize.py
+++ Anonymize.py
@@ -25,39 +25,44 @@ DISCIPLINE_AGGREGATION = {"-Planetary Science" : "Earth science",
 responses = pd.read_csv("DataPubSurveyResponses - Form Responses.csv")
 
 
+# drop unecessary demographic or revealing columns
+responses_ft = responses.drop(COLUMNS_TO_DROP, axis=1)
 
-#responses['discipline'] = responses['discipline'].map(DISCIPLINE_AGGREGATION)
+# checkboxes -------------------------------------------------------------------
 
-
-
+# filter checkbox columns to replace free text with 'Other' and split multiple
+# answers with '; '
+ 
 # replace any anwser not in the provided list with 'Other'
 def strip_other(cell, column):
-    if cell != cell:
-        return cell
-    else:
-        cell = pd.Series(cell)        
-        cell[~cell.isin(COLUMN_TO_ANSWERS[column])] = 'Other'
-        return cell
-
-
-
-# drop unecessary demographic or revealing columns
-
-responses_ft = responses.drop(COLUMNS_TO_DROP, axis=1)
+    cell = pd.Series(cell)        
+    cell[~cell.isin(COLUMN_TO_ANSWERS[column])] = 'Other'
+    return cell
 
 
 for column in CHECKBOX_COLUMNS:      
-
-    # split into individual answers on ','
+    # split answers on ',' into seires
     filtered_column = responses_ft[column].str.split(", ").dropna()
     
     # replace free-text with 'Other'    
     filtered_column = filtered_column.map(lambda x : strip_other(x, column))
     
-    filtered_column = filtered_column.map(lambda x : '; '.join(x))
+    # rejoin as string with new delimeter
+    filtered_column = filtered_column.map(lambda x : "; ".join(x))
     
+    # replace original column
     responses_ft[column] = filtered_column
 
+# discipline -------------------------------------------------------------------
+
+# merge disciplines with < 3 responders into larger classes
  
+def merge_disciplines(cell):
+    if cell in DISCIPLINE_AGGREGATION.keys():
+        cell = DISCIPLINE_AGGREGATION[cell]
+    return cell
+
+responses_ft['discipline'] = responses_ft['discipline'].map(merge_disciplines)
+
 responses_ft.to_csv('test_output.csv')
 # consolidate sparsely populated disciplines
