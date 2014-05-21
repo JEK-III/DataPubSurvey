d5621c4ddb04cdf59f581a9a3050e3fd610e9b72
John Kratz <qbxdev@gmail.com>
2014-05-21 15:00:26 -0700

    Made graphs vastly prettier

--- GraphPRDefByReviewExperience.py
+++ GraphPRDefByReviewExperience.py
@@ -8,9 +8,11 @@ Created on Fri May 16 11:52:29 2014
 ivar = 'researcher_review_experience'
 dvar = 'peer_review_definition'
 
-COLORS = ['#8dd3c7',
-          '#ffffb3']
+COLORS = ['#678bbc',
+          '#f8c690']
 
+TITLE_FONT={'name' : 'Helevetica',
+            'size' : 12}
 execfile("ReadInSurvey.py")
 
 # extract checkbox column and split responses into array
@@ -32,7 +34,10 @@ merged_responses = pd.merge(ivar_checkbox_responses, dvar_checkbox_responses,
 
 #new_df=pd.DataFrame()
 
-experience_dfs = []
+
+fig, subfigs = plt.subplots(2, 3, sharex=True, sharey=True)
+
+i = j = 0
 
 for action in REVIEW_ACTIONS:
     responses_by_action = pd.DataFrame(merged_responses.groupby(action).sum())
@@ -42,22 +47,45 @@ for action in REVIEW_ACTIONS:
     
     responses_by_action.ix[True] = responses_by_action.ix[True].apply(lambda x : x/total_counts[True])
     responses_by_action.ix[False] = responses_by_action.ix[False].apply(lambda x : x/total_counts[False])
-   
-    experience_dfs.append(responses_by_action)
+    responses_by_action = responses_by_action.T
+    
+    #plt.tick
+    responses_by_action.plot(kind='bar', 
+                             color=COLORS, 
+                             figure=fig,
+                             ax=subfigs[i][j], 
+                             ylim=(0,1),
+                             yticks=[0,0.5,1],
+                             grid=False, 
+                             legend=False,
+                             #title=action,
+                             edgecolor='none') 
+    subfigs[i][j].tick_params(axis='both', 
+                              which='both',
+                              bottom='off',
+                              top='off',
+                              right='off')
+                              
+    subfigs[i][j].set_title(action, fontdict=TITLE_FONT)
+    subfigs[i][j].spines['right'].set_visible(False)
+    subfigs[i][j].spines['top'].set_visible(False)
+    subfigs[i][j].spines['bottom'].set_color('#5b6f74')
+    subfigs[i][j].spines['left'].set_color('#5b6f74')
+                    
+    if (i == 0 and j == 2):
+        i += 1  
+        j = 0
+    else:
+        j += 1
+
+fig.suptitle('Data peer review definition as a function of review experience', fontsize=14)        
+
+fig.show()
+ #   experience_dfs.append(responses_by_action)
 
 
-combined_frame = pd.concat(experience_dfs, REVIEW_ACTIONS).T
-combined_frame = combined_frame.reindex(columns=[True, False])
-
-fig, subfigs = plt.subplots(2, 3, sharex=True, sharey=True)
-
-combined_frame.plot(kind='bar', 
-                    color=COLORS, 
-                    figure=fig,
-                    ax=subfigs[0][0], 
-                    ylim=[0,1],
-                    grid=False, 
-                    legend=False)
+#combined_frame = pd.concat(experience_dfs, REVIEW_ACTIONS).T
+#combined_frame = combined_frame.reindex(columns=[True, False])
 
 
-#test_group[True]
+
