e75aad8e91a17dab44453f313ae41f1ec3bc8f46
John Kratz <qbxdev@gmail.com>
2014-05-23 14:48:28 -0700

    GraphResearcherEvalbySharingExperience works

--- GraphResearcherEvalbySharingExperience.py
+++ GraphResearcherEvalbySharingExperience.py
@@ -14,12 +14,35 @@ IVAR_RESPONSES = COLUMN_TO_ANSWERS[IVAR]
 DVAR_RESPONSES =RESEARCHER_VALUE_SEQUENCE
 
 
-COLORS = ['#678bbc',
-          '#f8c690']
+
+COLORS = ['#08519c',
+          '#3182bd',
+          '#6baed6',
+          '#bdd7e7',
+          '#eff3ff',
+          '#006d2c',
+          '#31a354',
+          '#74c476',
+          '#bae4b3',
+          '#edf8e9']
+
+
+
+
+
 
 TITLE_FONT={'name' : 'Helevetica',
             'size' : 12}
 
+INDEX_LIST = []
+for i in DVARS:
+    INDEX_LIST.extend([i, i])
+
+
+GRAPH_INDEX = pd.MultiIndex.from_arrays(
+                                [len(DVARS) * [True, False],
+                                 INDEX_LIST], 
+                                 names = ['ivar', 'dvars'])
             
 execfile("ReadInSurvey.py")
 
@@ -40,7 +63,11 @@ fig, subfigs = plt.subplots(2, 3, sharex=True, sharey=True)
 i = j = 0
 
 for action in IVAR_RESPONSES:
+        
+    mask = merged_responses[IVAR].apply(lambda x: action in x)
                 
+    collected_counts = pd.DataFrame(index=DVAR_RESPONSES, columns=GRAPH_INDEX)        
+    
     
     #responses_by_action = pd.DataFrame(merged_responses.groupby(action).sum())
     #total_counts = pd.value_counts(ivar_checkbox_responses[action])
@@ -48,17 +75,23 @@ for action in IVAR_RESPONSES:
 #    responses_by_action = responses_by_action.reindex([True, False])    
     
     #mask = merged_responses.groupby(action in merged_responses[IVAR])
-    test_thing = merged_responses.groupby(IVAR)    
     
-    counts = {}
-    for column in DVARS:
-      counts[column] = responses_by_action_true[column].value_counts().dropna()
-      
-    dvar_leikert_responses = pd.DataFrame(counts)
-    dvar_leikert_responses = dvar_leikert_responses.transpose().fillna(0)
-            
-            
+    #counts = {}
+    for column in DVARS:            
+        collected_counts[True, column] = \
+            merged_responses[mask][column].value_counts().fillna(0)
+        collected_counts[False, column] = \
+            merged_responses[~mask][column].value_counts().fillna(0)
+    
+    collected_counts = collected_counts.transpose() 
+     
+    #collected_counts[True] = collected_counts[True].div(collected_counts[True].sum(1).astype(float), axis = 0)
+    #collected_counts[False] = collected_counts[False].div(collected_counts[False].sum(1).astype(float), axis = 0)
+    collected_counts = collected_counts.div(collected_counts.sum(1).astype(float), axis = 0)
 
+            
+            
+    """
     for answer in [True, False]:
         if answer in total_counts.index:
             responses_by_action.ix[answer] = (
@@ -66,12 +99,12 @@ for action in IVAR_RESPONSES:
                     x/total_counts[answer]))
         else: 
             total_counts = total_counts.append(pd.Series({answer : 0}))
-          
+    """     
 
     #responses_by_action = responses_by_action.T
     
     #plt.tick
-    dvar_leikert_responses.plot(kind='barh',
+    collected_counts.plot(kind='barh',
                                 stacked=True, 
                              color=COLORS, 
                              figure=fig,
@@ -92,7 +125,7 @@ for action in IVAR_RESPONSES:
     subfigs[i][j].spines['bottom'].set_color('#5b6f74')
     subfigs[i][j].spines['left'].set_color('#5b6f74')
                     
-                        
+    """                    
     n_string = ("n =\n" + str(total_counts[True]) + " have done\n" + 
         str(total_counts[False]) + " have not done")
     
@@ -101,7 +134,7 @@ for action in IVAR_RESPONSES:
                        fontsize=10,
                        horizontalalignment='left',
                        transform=subfigs[i][j].transAxes)
-    
+    """
     if (i == 0 and j == 2):
         i += 1  
         j = 0
