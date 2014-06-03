25b2b4b02ee20ba0ffc2a5e1447b0cc1cd081440
John Kratz <qbxdev@gmail.com>
2014-06-03 13:58:31 -0700

    Minor adjustments to make more generic

--- GraphCheckbox.py
+++ GraphCheckbox.py
@@ -22,20 +22,29 @@ COLORS = ['a6cee3',
 """
 COLORS = 'w'
 
-
+ANSWERS = COLUMN_TO_ANSWERS[dvar]
 
 # extract checkbox column and split responses into array
 split_checkbox = responses[dvar].str.split("; ").dropna()
 
 # DF of bools; responders x checkbox (checked = True) 
 checkbox_responses = pd.DataFrame({name : 
-    split_checkbox.apply(lambda x: name in x) for name in REVIEW_ACTIONS})
+    split_checkbox.apply(lambda x: name in x) for name in ANSWERS})
 
 # count checked boxes 
 response_counts = checkbox_responses.sum()
 
+response_counts = response_counts.apply(lambda x: 
+                                        float(x) / len(checkbox_responses))
+
+
 # sort descending for graph
-response_counts.sort(ascending=False)
+response_counts.sort(ascending=True)
 
 # plot bar graph
-response_counts.plot(kind='bar', alpha=1, color=COLORS)
+response_counts.plot(kind='barh', 
+                     alpha=1, 
+                     color=COLORS,
+                     grid=False,
+                     title=dvar,
+                     xlim=(0,1))
