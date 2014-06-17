a75eb6d2642f09541f0749938b4a187d92dd18bb
John Kratz <qbxdev@gmail.com>
2014-06-17 14:17:57 -0700

    Get checkbox Chi-squared testing going

--- CheckboxIndependenceTest.py
+++ CheckboxIndependenceTest.py
@@ -5,7 +5,8 @@ Created on Tue Jun 17 09:52:32 2014
 @author: jkratz
 """
 import scipy.stats as sps
-
+execfile("ReadInSurvey.py")
+execfile("DefineConstants.py")
 ANSWERS = COLUMN_TO_ANSWERS[dvar]
 
 
@@ -19,20 +20,30 @@ checkbox_responses = pd.DataFrame({name :
 # count checked boxes 
 response_counts = checkbox_responses.sum()
 
+file_title = 'chi2_independence_' + dvar + '.csv'
+pvalues = pd.DataFrame(index=ANSWERS, columns=ANSWERS)
+
+
+i=0
 for a in ANSWERS:
+    i +=1    
 
     a_t = checkbox_responses[checkbox_responses[a] == True]
     a_f = checkbox_responses[checkbox_responses[a] == False]
 
-    for b in ANSWERS:
-        df = pd.DataFrame(index=[True, False], columns=[True, False])
-        df[True] = a_t[b].value_counts()
-        df[False] = a_f[b].value_counts()
+    for b in ANSWERS[i:]:
+        square = pd.DataFrame(index=[True, False], columns=[True, False])
+        square[True] = a_t[b].value_counts()
+        square[False] = a_f[b].value_counts()
 
-        chi2, p, df, expected = sps.chi2_contingency(df.as_matrix())
+        chi2, p, df, expected = sps.chi2_contingency(square.as_matrix())
+        
+        pvalues.ix[a,b] = p
        
         if chi2 == chi2:
             print a + " x " + b + ": chi2= " + str(chi2) + " p= " + str(p)
             print expected
-        
+            relationship = square.as_matrix() - expected
+            print relationship[0][0]
 
+pvalues.to_csv(file_title)
