e7480ed0facb607ec8c3739d2ac894b0893b3dfd
John Kratz <qbxdev@gmail.com>
2014-07-11 11:06:59 -0700

    Finish Discussion:Data publication

--- GraphAllLikertGridsPlusMean.py
+++ GraphAllLikertGridsPlusMean.py
@@ -76,7 +76,7 @@ for question in QUESTIONS:
     collected_counts = collected_counts.div(collected_counts.sum(1).astype(float), 
                                             axis = 0)      
                                             
-    #print collected_counts
+    print collected_counts
                                         
     collected_counts = collected_counts.reindex(
         index=collected_counts.index[ ::-1 ])
